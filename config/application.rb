1a148f7052a7c931a12ef207398b4075a9bf584f
tagu kana <taguno180n@gmail.com>
2018-09-29 14:23:12 +0000

    投稿日を表示

--- config/application.rb
+++ config/application.rb
@@ -13,5 +13,8 @@ module Flyer
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.active_record.default_timezone = :local
+    config.time_zone = 'Tokyo'
   end
 end
